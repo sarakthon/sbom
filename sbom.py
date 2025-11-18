ced7719fae28da130966320093c4ca14b8c4d7c2
sarakthon <189987357+sarakthon@users.noreply.github.com>
2025-11-18 19:18:48 +0100

    Method for writing to .csv-file "sbom.csv"

--- sbom.py
+++ sbom.py
@@ -1,3 +1,4 @@
+import csv
 import json
 import sys
 from pathlib import Path
@@ -23,9 +24,9 @@ def read_json(json_path: Path):
     with open(json_path) as f:
         data = json.load(f)
 
+    # Parse dependencies and DevDependencies from package.json
     deps = data.get("dependencies", {})
     devDeps = data.get("DevDependencies", {})
-
     allDeps = deps | devDeps
 
     for name, version in allDeps.items():
@@ -33,17 +34,23 @@ def read_json(json_path: Path):
     
     return dependencies
 
-        
+def write_to_csv(directory_path: Path, dependencies):
+    output_file = directory_path / "sbom.csv"
 
-def write_to_csv():
-    ...
+    with open(output_file, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["name", "version", "type", "absolute path"])
+        writer.writerows(dependencies)
+
 
 def write_to_json():
     ...
 
 def main():
     directory_path = Path(sys.argv[1])
+
     repo_counter = 0
+    dependencies = []
 
     for repo in directory_path.iterdir():
         repo_counter += 1
@@ -53,12 +60,18 @@ def main():
             json = repo / "package.json"
 
             if txt.exists():
-                read_txt(txt)
+                txt_deps = read_txt(txt)
+                for name, version in txt_deps:
+                    dependencies.append((name, version, "npm", repo))
             
             if json.exists():
-                print(".json exists")
+                json_deps =read_json(json)
+                for name, version in json_deps:
+                    dependencies.append((name, version, "pip", repo))
 
     print(f"Found {repo_counter} repositories in {directory_path}")
+    
+    csv = write_to_csv(directory_path, dependencies)
 
 if __name__ == "__main__":
     main()
